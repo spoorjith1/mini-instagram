a4f301bbb5335b136f3684e407655db07ef5c0af
spoorjith1 <spoorjith1@gmail.com>
2026-04-21 13:09:30 +0530

    frontend home page

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <footer className='footer py-3 my-3'>
+      <hr className=' border-bottom' />
+      <h4 className='copy-right'>Z Lite &copy; 2026</h4>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <>
+    <div className='container'>
+      <p>Explore the Z Lite app now</p>
+      <p>Find people connect with new people share your stories and make new friends :)</p>
+    </div>
+    </>
+  )
+}
+
+export default Main
